5c1487e31be4d76db9a1ef1dc773547e5c24b9e5
Ubuntu <ubuntu@ip-10-0-0-7.us-west-2.compute.internal>
2017-09-29 23:13:28 +0000

    spark streaming implemented for both stock and twitter

--- cssTwitterST.py
+++ cssTwitterST.py
@@ -14,6 +14,7 @@ from uuid import uuid1
 from pyspark_cassandra import CassandraSparkContext
 
 from pyspark.sql import SQLContext
+import datetime
 
 reload(sys)
 sys.setdefaultencoding('utf-8')
@@ -38,16 +39,19 @@ def tweetwithSentiment(tweet,pwords, nwords, sterms):
     text = tweet.get(u'text')
     if text is not None:
          words = text.split(" ")
-         sentiment = np.sign(sum([(1 if word in pwords else 1 if word in nwords else 0)  for word in words]))
+         sentiment = float(np.sign(sum([(1 if word in pwords else 1 if word in nwords else 0)  for word in words])))
          
          stermsBool = [sterm in words for sterm in sterms]
+         # tweet = {"key": {}}
          # print(type(tweet))
          tweet["sentiment"]=sentiment
          tweet["searchTermBool"]=stermsBool
+         
+         #print(tweet)
          return (tweet)
          #outJson={"tweet": text, "searchTermBool":stermsBool}
          #return (outJson)
-    outJson = {"tweet": " ", "searchTermBool": 0}
+    outJson = {"tweet": "blank", "searchTermBool": 0}
     return outJson
 
 def searchTermFunction(tweet, sterms): # give set of tuples for each tweet with each element for search term
@@ -57,12 +61,14 @@ def searchTermFunction(tweet, sterms): # give set of tuples for each tweet with 
       		if tweet.get(u"searchTermBool")[ind]:
         		searchTermSentimentsLs.append((sterm,tweet.get(u"sentiment")))   	    
     	#print(searchTermSentimentsLs)
+    #searchTermSentimentsLs.append(("insertion_time","sentiment")))
     return searchTermSentimentsLs
-
+    #return ("none",0)
 def main():
     pwords = load_wordlist("./Dataset/positive.txt")
     nwords = load_wordlist("./Dataset/negative.txt")
-    sterms = ["the","in"]
+    sterms = load_wordlist("./Dataset/keyWords.txt")
+    #print(len(sterms))
     conf = SparkConf().\
         setMaster("local[2]").\
         setAppName("TweeStreamer").\
@@ -70,7 +76,7 @@ def main():
         "52.25.173.31, 35.165.251.179, 52.27.187.234, 52.38.246.84")
     sc = CassandraSparkContext(conf=conf)
     sc.setLogLevel("WARN")
-    sql = SQLContext(sc)
+    #sql = SQLContext(sc)
     # Creating a streaming context with batch interval of 1 sec
     ssc = StreamingContext(sc, 10)
     ssc.checkpoint("checkpoint")
@@ -85,16 +91,20 @@ def main():
     tweets.count().map(lambda x:'Tweets in this batch: %s' % x).pprint() 
     tweetsUsentiment = tweets.map(lambda tweet: tweetwithSentiment(tweet, pwords, nwords, sterms))
     #tweetsUsentiment.pprint()
-
+    #insert_time = 
     searchTermUsentiment = tweetsUsentiment.flatMap(lambda tweet: searchTermFunction(tweet, sterms)).reduceByKey(lambda a, b: a+b)
+    searchTermUsentiment = searchTermUsentiment.map(lambda (key, value): {"searchterm":"_"+key, "insertion_time": datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S'), "sentiment": value})
     searchTermUsentiment.pprint()
-    
+    #rddVal = sc.parallelize([{
+    #	"key": "k",
+    #	"stamp": 1}])
+    #rddVal.pprint()
     #tweetsUsentiment.saveToCassandra("tweetdb", "tweettable") 
-    #searchTermUsentiment.saveToCassandra("tweetdb","searchTermsTimeSeries")
-    #searchTermSentiment = tweetsUsentiment.map(lambda tweet: searchTermFunction(tweet,sterms))
+    searchTermUsentiment.saveToCassandra("tweetdb","searchtermtable")
+    # searchTermSentiment = tweetsUsentiment.map(lambda tweet: searchTermFunction(tweet,sterms))
      
     ssc.start()
-    ssc.awaitTerminationOrTimeout(10000)
+    ssc.awaitTerminationOrTimeout(1000)
     ssc.stop(stopGraceFully = True)
 
     
--- kTwitterProducer.py
+++ kTwitterProducer.py
@@ -25,7 +25,7 @@ class StdOutListener(StreamListener):
     def on_error(self, status):
         print status
 
-WORDS_TO_TRACK = "the to and is in it you of for on my that at with me do have just this be so are not was but out up what now new from your like good no get all about we if time as day will one how can some an am by going they go or has know today there love more work too got he back think did when see really had great off would need here thanks been still people who night want why home should well much then right make last over way does getting watching its only her post his morning very she them could first than better after tonight our again down news man looking us tomorrow best into any hope week nice show yes where take check come fun say next watch never bad free life".split()
+#WORDS_TO_TRACK = "the to and is in it you of for on my that at with me do have just this be so are not was but out up what now new from your like good no get all about we if time as day will one how can some an am by going they go or has know today there love more work too got he back think did when see really had great off would need here thanks been still people who night want why home should well much then right make last over way does getting watching its only her post his morning very she them could first than better after tonight our again down news man looking us tomorrow best into any hope week nice show yes where take check come fun say next watch never bad free life".split()
 
 if __name__ == '__main__':
     print 'running the twitter-stream python code'
